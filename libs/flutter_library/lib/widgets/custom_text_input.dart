6a964d0a59af9c96f0320617e85e51a949c5313c
hayam-tarek <hayamtareq28@gmail.com>
2024-11-06 17:23:13 +0200

    create password view done

--- libs/flutter_library/lib/widgets/custom_text_input.dart
+++ libs/flutter_library/lib/widgets/custom_text_input.dart
@@ -2,30 +2,37 @@ import 'package:flutter/material.dart';
 import 'package:flutter_library/constants/app_color.dart';
 import 'package:flutter_library/constants/app_spacing.dart';
 
-class PhoneNumberTextFormField extends StatelessWidget {
-  const PhoneNumberTextFormField({
+class CustomTextInput extends StatelessWidget {
+  const CustomTextInput({
     super.key,
     required this.controller,
     this.onChanged,
+    this.hintText,
+    this.keyboardType,
+    this.obscureText = false,
   });
 
   final TextEditingController controller;
   final void Function(String)? onChanged;
+  final String? hintText;
+  final TextInputType? keyboardType;
+  final bool obscureText;
 
   @override
   Widget build(BuildContext context) {
     return SizedBox(
       height: 60,
       child: TextFormField(
+        obscureText: obscureText,
         controller: controller,
         onChanged: onChanged,
-        expands: true,
-        maxLines: null,
+        expands: obscureText ? false : true,
+        maxLines: obscureText ? 1 : null,
         minLines: null,
-        keyboardType: TextInputType.phone,
-        decoration: const InputDecoration(
-          hintText: 'رقم الهاتف ...',
-          border: OutlineInputBorder(
+        keyboardType: keyboardType,
+        decoration: InputDecoration(
+          hintText: hintText,
+          border: const OutlineInputBorder(
             borderSide: BorderSide(
               color: AppColor.borderDefaultColor,
             ),
