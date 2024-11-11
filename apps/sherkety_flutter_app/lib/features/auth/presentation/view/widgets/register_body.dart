0a2251cddb1fb25844ae8cecc09dcfa812c3f227
hayam-tarek <hayamtareq28@gmail.com>
2024-11-11 02:14:46 +0200

    firbase config done

--- apps/sherkety_flutter_app/lib/features/auth/presentation/view/widgets/register_body.dart
+++ apps/sherkety_flutter_app/lib/features/auth/presentation/view/widgets/register_body.dart
@@ -1,10 +1,12 @@
+import 'dart:developer';
+
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_library/ui_lib.dart';
 import 'package:sherkety_flutter_app/core/shared/widgets/base_spacing.dart';
 import 'package:sherkety_flutter_app/core/theme/styles.dart';
 import 'package:sherkety_flutter_app/features/auth/presentation/view/verify_phone_view.dart';
 import 'package:sherkety_flutter_app/features/auth/presentation/view/widgets/contacts.dart';
-import 'package:sherkety_flutter_app/features/auth/presentation/view/widgets/terms_of_service_privacy_policy.dart';
 import 'package:sherkety_flutter_app/features/auth/presentation/view/widgets/text_register_with.dart';
 
 class RegisterBody extends StatelessWidget {
@@ -13,6 +15,8 @@ class RegisterBody extends StatelessWidget {
   });
 
   final TextEditingController controller = TextEditingController();
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  String _verificationId = '';
 
   @override
   Widget build(BuildContext context) {
@@ -25,11 +29,30 @@ class RegisterBody extends StatelessWidget {
           const BaseSpacing(),
           PhoneNumber(controller: controller),
           const BaseSpacing(),
-          const TermsOfServicePrivacyPolicy(),
+          // const TermsOfServicePrivacyPolicy(),
           const BaseSpacing(),
           DefaultButton(
             text: 'دخول',
-            onPressed: () {
+            onPressed: () async {
+              try {
+                await _auth.verifyPhoneNumber(
+                  phoneNumber: '+201116289719',
+                  verificationCompleted:
+                      (PhoneAuthCredential credential) async {
+                    await _auth.signInWithCredential(credential);
+                    log('sing in with credential');
+                  },
+                  verificationFailed: (FirebaseAuthException e) {
+                    log('Verification failed: ${e.message}');
+                  },
+                  codeSent: (String verificationId, int? resendToken) {
+                    log('Code sent to hayam');
+                  },
+                  codeAutoRetrievalTimeout: (String verificationId) {},
+                );
+              } on Exception catch (e) {
+                log(e.toString());
+              }
               Navigator.push(
                 context,
                 MaterialPageRoute(
--- apps/sherkety_flutter_app/lib/main.dart
+++ apps/sherkety_flutter_app/lib/main.dart
@@ -1,11 +1,18 @@
 import 'package:country_code_picker/country_code_picker.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:sherkety_flutter_app/core/constants/asset_colors.dart';
 import 'package:sherkety_flutter_app/core/constants/asset_fonts.dart';
 import 'package:sherkety_flutter_app/features/auth/presentation/view/register_view.dart';
 
-void main() {
+import 'firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const SherketyApp());
 }
 
